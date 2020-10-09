aa57bc6657f8c331f13df037818a5ce752ebe6d5
Joon Lee <l33j00n325@gmail.com>
2020-10-09 14:47:48 -0700

    read image with tesseractjs

--- src/components/ImageReader.vue
+++ src/components/ImageReader.vue
@@ -5,21 +5,43 @@
     @change="readFile"
   >
   <p>
-    <img :src="src" />
+    <img :src="src" style="width:100%" />
+  </p>
+  <p>
+    {{ imageText }}
   </p>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+  import { ref, onMounted, onBeforeUnmount } from 'vue'
+  import { createWorker } from 'tesseract.js'
 
   export const src = ref('')
+  export const imageText = ref('')
+
   const reader = new FileReader()
   const image = document.createElement('img')
+  const worker = createWorker({
+    logger: m => console.log(m)
+  })
+
+  onMounted(async () => {
+    await worker.load()
+    await worker.loadLanguage('eng')
+    await worker.initialize('eng')
+  })
+
+  onBeforeUnmount(async () => {
+    await worker.terminate()
+  })
 
   reader.onload = async event => {
     const imageSrc = event.target.result
     image.src = imageSrc
     src.value = imageSrc
+
+    const { data: { text } } = await worker.recognize(imageSrc)
+    imageText.value = text
   }
 
   export function readFile (event) {
