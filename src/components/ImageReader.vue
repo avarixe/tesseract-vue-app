e84b6ef2cabc91e1da7586a8300e786c1a0ff5cd
Joon Lee <l33j00n325@gmail.com>
2020-10-09 10:59:37 -0700

    add vue3 setup script

--- src/components/ImageReader.vue
+++ src/components/ImageReader.vue
@@ -9,27 +9,20 @@
   </p>
 </template>
 
-<script>
+<script setup>
   import { ref } from 'vue'
 
-  export default {
-    name: 'ImageReader',
-    setup () {
-      const src = ref('')
-      const reader = new FileReader()
-      const image = document.createElement('img')
+  export const src = ref('')
+  const reader = new FileReader()
+  const image = document.createElement('img')
 
-      reader.onload = async event => {
-        const imageSrc = event.target.result
-        image.src = imageSrc
-        src.value = imageSrc
-      }
+  reader.onload = async event => {
+    const imageSrc = event.target.result
+    image.src = imageSrc
+    src.value = imageSrc
+  }
 
-      function readFile (event) {
-        reader.readAsDataURL(event.target.files[0])
-      }
-
-      return { src, readFile }
-    }
+  export function readFile (event) {
+    reader.readAsDataURL(event.target.files[0])
   }
 </script>
